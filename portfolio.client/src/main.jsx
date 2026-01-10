50a0ab7dbb4d067dc55810e1fd2d8eea6440103e
Tredir <tredirs@gmail.com>
2026-01-10 13:49:22 +0200

    added vercel analytics

--- portfolio.client/src/main.jsx
+++ portfolio.client/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import mermaid from 'mermaid'
 import './index.css'
 import App from './App.jsx'
@@ -20,6 +21,7 @@ mermaid.initialize({
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Analytics />
     <App />
   </StrictMode>,
 )
